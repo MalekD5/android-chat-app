16c00fd530ba62a2b282c236535693c34e8eead6
Malek <106095300+MalekD5@users.noreply.github.com>
2025-05-28 16:44:25 +0300

    formatting

--- app/src/main/java/com/example/chatapp/DbHelper.java
+++ app/src/main/java/com/example/chatapp/DbHelper.java
@@ -5,22 +5,29 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-class DbHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "search.db";
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "database.db";
     private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_NAME = "infos";
+    public static final String TABLE_NAME = "messages";
     public static final String COLUMN_USER_ID = "userID";
     public static final String COLUMN_GENDER = "gender";
     public static final String COLUMN_TITLE = "title";
     public static final String COLUMN_BODY = "body";
 
-    private static final String SQL_CREATETABLE =
-            "CREATE TABLE " + TABLE_NAME + " (" +
-                    COLUMN_USER_ID + " INTEGER PRIMARY KEY," +
-                    COLUMN_GENDER + " TEXT," +
-                    COLUMN_TITLE + " TEXT," +
-                    COLUMN_BODY + " TEXT)";
+    private static final String SQL_CREATE_TABLE = String.format(
+            "CREATE TABLE %s (" +
+                    "%s INTEGER PRIMARY KEY, " +
+                    "%s TEXT, " +
+                    "%s TEXT, " +
+                    "%s TEXT)",
+            TABLE_NAME,
+            COLUMN_USER_ID,
+            COLUMN_GENDER,
+            COLUMN_TITLE,
+            COLUMN_BODY
+    );
 
     public DbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -28,17 +35,18 @@ class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQL_CREATETABLE);
+        db.execSQL(SQL_CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
 
     public Cursor viewAll() {
-        SQLiteDatabase db = this.getReadableDatabase();
-        return db.query(TABLE_NAME, new String[]{COLUMN_USER_ID, COLUMN_TITLE}, null, null, null, null, null);
+        SQLiteDatabase db = getReadableDatabase();
+        String[] projection = { COLUMN_USER_ID, COLUMN_TITLE };
+        return db.query(TABLE_NAME, projection, null, null, null, null, null);
     }
-
 }
+
 
 
